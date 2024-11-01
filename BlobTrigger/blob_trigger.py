368924ceaf34da1c318ec20933f2ad8820ffcbf9
claraibarzabal <141688090+claraibarzabal@users.noreply.github.com>
2024-11-01 16:18:47 -0300

    Delete BlobTrigger directory

--- BlobTrigger/blob_trigger.py
+++
@@ -1,9 +0,0 @@
-import logging
-import azure.functions as func
-
-app = func.FunctionApp()
-
-@app.function_name(name="BlobTrigger1")
-@app.blob_trigger(arg_name="myblob", path="samples/{name}", connection="AzureWebJobsStorage")
-def blob_trigger(myblob: func.InputStream) -> None:
-    logging.info(f'Blob trigger processed blob: {myblob.name}, Size: {myblob.length} bytes')
