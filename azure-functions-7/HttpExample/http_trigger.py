2751b310b041c3173df9e4a7853d2a1350ed950a
claraibarzabal <141688090+claraibarzabal@users.noreply.github.com>
2024-11-01 23:28:00 -0300

    Update http_trigger.py

--- azure-functions-7/HttpExample/http_trigger.py
+++ azure-functions-7/HttpExample/http_trigger.py
@@ -4,18 +4,19 @@ import azure.functions as func
 app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
-@app.route(route="http_example")
+@app.route(route="req")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Processing HTTP request')
-    name = req.params.get('name')
     
-    if not name:
+    user = req.params.get("user")
+    if not user:
         return func.HttpResponse(
-            "Please pass a name on the query string",
-            status_code=400
+            "Please pass a user name in the query string.",
+            status_code=400  # Bad Request
         )
     
-    return func.HttpResponse(f"Hello, {name}!")
+    return func.HttpResponse(f"Hello, {user}!", status_code=200)
+
 
 
 """ import logging
